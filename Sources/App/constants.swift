520aa29cf99cffa1d17141394deecc92e39c23e5
Jonathan Bennett <jonathan@jbennett.me>
2020-04-22 08:47:24 -0400

    still playing with values

--- Sources/App/constants.swift
+++ Sources/App/constants.swift
@@ -7,12 +7,13 @@
 import Vapor
 
 enum Constants {
-    
-    static let foodSeeking = 1000
-    static let moveBias = 10
+
     static let avoidWalls = 100000
-    static let avoidSnakeHeads = 500
     static let avoidSnakeBodies = 100000
+    static let avoidSnakeHeads = 500
+    static let moveBias = 20 // 5 moves trumps a food?
+    static let foodSeeking = 100
+    
     
     static var isMulticolored: Bool = {
         return Environment.get("env") != "prod"
