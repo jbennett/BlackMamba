42cbd0b139e67c7be3a8db218cfaa6d57c6b6e21
Jonathan Bennett <jonathan@jbennett.me>
2020-04-26 20:38:53 -0400

    wall avoidance

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -15,21 +15,58 @@ public func routes(_ router: Router) throws {
         let colors = ["#4d9dff", "#dabaff", "#fe806f", "#292a30", "#6e7883", "#ff8170", "#1d2022", "#1c4877", "#007aff"]
         let color = Constants.isMulticolored ? colors.random! : "#f5af10"
         
-        return """
+//        return """
+//{
+//        "color": "\(color)",
+//    "headType": "silly",
+//    "tailType": "bolt"
+//}
+//"""
+        
+     return """
 {
-        "color": "\(color)",
-    "headType": "silly",
-    "tailType": "bolt"
+        "color": "#f52627",
+    "headType": "fang",
+    "tailType": "hook"
 }
 """
     }
     
     router.post(MoveData.self, at: "/move") { req, move -> String in
 //        logger = { print("Turn \(move.turn): \($0)") }
-        let simulator = BlackMamba()
+//        let simulator = BlackMamba()
 //        logger("\(move.you.head): \(simulator.getMove(move.you, board: move.board))")
+//        return """
+//        { "move": "\(simulator.getMove(move.you, board: move.board).0)" }
+//"""
+        
+        
+        var direction = "up"
+        
+//        if snakre is at top go atLeft
+        if move.you.head.y == 0 {
+            direction = "left"
+        }
+        
+        if move.you.head.x == 0 {
+            direction = "down"
+        }
+
+        if (move.you.head.y == move.board.height - 1) {
+            direction = "right"
+        }
+
+        if (move.you.head.x == move.board.width - 1) &&
+            (move.you.head.y != 0) {
+            direction = "up"
+        }
+        
+        
+        
+//        direction = ["up", "down", "left", "right"].rasndom!
+        
         return """
-        { "move": "\(simulator.getMove(move.you, board: move.board).0)" }
+{ "move": "\(direction)" }
 """
     }
 
